d776a9e17da4225027f99c89376304a5ebd0abae
Mateusz BIS <Mateuszbis85@gmail.com>
2021-02-25 10:15:22 +0100

    style: :art: format the /lang directory

--- packages/redinn_core/lang/en.js
+++ packages/redinn_core/lang/en.js
@@ -1,10 +1,10 @@
 export default {
-    errors: {
-        "404": "page not found",
-        "403": "permission denied",
-        "500": "internal server error",
-        "503": "service unavailable",
-        "504": "gateway timeout"
-    },
-    message: "Error"
-}
+  errors: {
+    404: "page not found",
+    403: "permission denied",
+    500: "internal server error",
+    503: "service unavailable",
+    504: "gateway timeout",
+  },
+  message: "Error",
+};
