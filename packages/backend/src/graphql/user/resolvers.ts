162fffeecf910bc757f4dd7691a0b7d9e957654b
Matb85 <69219238+Matb85@users.noreply.github.com>
2021-06-29 14:50:38 +0200

    chore(api): :arrow_up: upgrade dependencies

--- packages/backend/src/graphql/user/resolvers.ts
+++ packages/backend/src/graphql/user/resolvers.ts
@@ -1,23 +1,18 @@
-import User, { UserI } from "@/db/user";
+import User, { UserDoc, UserI } from "@/db/user";
 import { Context } from "../index";
 
 type SafeUser = Omit<UserI, "password">;
 
 export default {
   Query: {
-    user(_: unknown, __: unknown, context: Context): Promise<SafeUser> {
-      return User.findById(context.user)
-        .then(
-          (user: UserI): SafeUser => {
-            return {
-              email: user.email,
-              firstName: user.firstName,
-              lastName: user.lastName,
-              enterprises: user.enterprises,
-            };
-          }
-        )
-        .catch((err: Error) => err);
+    async user(_: unknown, __: unknown, context: Context): Promise<SafeUser> {
+      const user = (await User.findById(context.user)) as UserDoc;
+      return {
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        enterprises: user.enterprises,
+      };
     },
   },
 };
