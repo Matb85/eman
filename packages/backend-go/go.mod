55a7715fc792b28fd4bf42d3109a836b44ef76c3
Matb85 <69219238+Matb85@users.noreply.github.com>
2021-07-02 20:55:56 +0200

    feat(api): :sparkles: graphql playground

--- packages/backend-go/go.mod
+++ packages/backend-go/go.mod
@@ -2,4 +2,7 @@ module redinnlabs.com/redinn-core
 
 go 1.16
 
-require github.com/graph-gophers/graphql-go v1.1.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/graph-gophers/graphql-go v1.1.0 // indirect
+)
--- packages/backend-go/src/main.go
+++ packages/backend-go/src/main.go
@@ -5,13 +5,24 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"redinnlabs.com/redinn-core/graphql"
 )
 
 func main() {
 	PORT := os.Getenv("PORT")
+	if len(PORT) == 0 {
+		PORT = "8080"
+	}
 	fmt.Println("listening on port " + PORT)
 
-	http.Handle("/graphql", graphql.Setup())
-	http.ListenAndServe(":"+PORT, nil)
+	router := mux.NewRouter()
+
+	g := graphql.Setup()
+	const GQLPATH = "/graphql/"
+
+	router.HandleFunc(GQLPATH, g.ServeHTTP).Methods("POST")
+	router.Methods("GET").PathPrefix(GQLPATH).Handler(http.StripPrefix(GQLPATH, http.FileServer(http.Dir("./static"))))
+
+	http.ListenAndServe(":"+PORT, router)
 }
