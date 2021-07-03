3a7b3fbf01ccb2d8deb1b96fb20c2a12aeb4d271
Matb85 <69219238+Matb85@users.noreply.github.com>
2021-07-03 16:20:38 +0200

    feat(api): :sparkles: types for users

--- packages/backend-go/graphql/main.go
+++ packages/backend-go/graphql/main.go
@@ -1,22 +1,34 @@
 package graphql
 
 import (
+	"io/ioutil"
+
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
 )
 
-
-type query struct{}
-
-func (*query) Hello() string { return "Hello, world!" }
+type rootquery struct {
+	UserQuery
+}
 
 func Setup() *relay.Handler {
-	
-	s := `
-			type Query {
-					hello: String!
-			}
-	`
-	schema := graphql.MustParseSchema(s, &query{})
+	// stich schemas
+	s := ""
+	files, err := ioutil.ReadDir("./graphql/schema")
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(files); i++ {
+		content, e := ioutil.ReadFile("./graphql/schema/" + files[i].Name())
+		if e != nil {
+			panic(e)
+		}
+		s += string(content)
+	}
+
+	// setup the graphql package
+	opts := []graphql.SchemaOpt{graphql.UseFieldResolvers()}
+	schema := graphql.MustParseSchema(s, &rootquery{}, opts...)
 	return &relay.Handler{Schema: schema}
 }
---
+++ packages/backend-go/graphql/user.resolvers.go
@@ -0,0 +1,21 @@
+package graphql
+
+import (
+	"context"
+
+	"github.com/graph-gophers/graphql-go"
+)
+
+type UserQuery struct{}
+
+func (r *UserQuery) User(ctx context.Context) (*User, error) {
+	return &User{"", "fdsfsd", "cos", "cos", &[]graphql.ID{"fdads"}}, nil
+}
+
+type User struct {
+	ID          graphql.ID
+	EMAIL       string
+	FIRSTNAME   string
+	LASTNAME    string
+	ENTERPRISES *[]graphql.ID
+}
