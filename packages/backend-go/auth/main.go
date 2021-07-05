86606d36f3ab757357fb1f767f6056e8b485d686
Matb85 <69219238+Matb85@users.noreply.github.com>
2021-07-05 18:04:42 +0200

    feat(api): :building_construction: db package

--- packages/backend-go/auth/main.go
+++ packages/backend-go/auth/main.go
@@ -1,14 +1,11 @@
 package auth
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"redinnlabs.com/redinn-core/database"
 )
 
 func SendResponse(w http.ResponseWriter, status int, message *map[string]string) {
@@ -18,13 +15,7 @@ func SendResponse(w http.ResponseWriter, status int, message *map[string]string)
 
 func Setup(router *mux.Router) {
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		panic(err)
-	}
-	db := client.Database("test")
+	db := database.Connect()
 
 	router.Use(jsonHeader)
 	router.HandleFunc("/register", Register(db)).Methods("POST")
