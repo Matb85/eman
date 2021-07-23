55332d61bd5f4301d3607226acc058204bbfd35c
Matb85 <69219238+Matb85@users.noreply.github.com>
2021-07-23 16:48:14 +0200

    fix(website): :bug: conditions for fetching enterprises

--- packages/frontend/plugins/authFetchEnterprise.ts
+++ packages/frontend/plugins/authFetchEnterprise.ts
@@ -3,8 +3,8 @@ import type { Auth, RecursivePartial, ModuleOptions } from "@nuxtjs/auth-next/di
 import { EnterpriseI, UserI } from "@/helpers/authTypes";
 
 export default async function ({ $auth, $axios, route }: Context) {
-  if (!$auth.loggedIn || typeof parseInt(route.params.enterprise) !== "number") return;
-
+  console.log();
+  if (!$auth.loggedIn || isNaN(parseInt(route.params.enterprise))) return;
   const response = await $axios.$post("/graphql", {
     query: `query get{enterprise(index: ${route.params.enterprise}){ name address {country zipcode city street }}}`,
   });
