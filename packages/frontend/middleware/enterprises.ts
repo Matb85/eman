bb807ecc212b0a098504d078b21faade5bc7d40a
Matb85 <69219238+Matb85@users.noreply.github.com>
2021-07-23 16:47:27 +0200

    fix(website): :truck: addbusiness -> addenterprise

--- packages/frontend/middleware/enterprises.ts
+++ packages/frontend/middleware/enterprises.ts
@@ -2,7 +2,7 @@ import { Middleware } from "@nuxt/types";
 
 const enterprises: Middleware = ({ $auth, redirect, route }) => {
   if (!$auth.user) return;
-  if ($auth.user.enterprises.length === 0 && !route.name?.includes("addbusiness")) redirect("/addbusiness");
+  if ($auth.user.enterprises.length === 0 && !route.name?.includes("addenterprise")) redirect("/addenterprise");
 };
 
 export default enterprises;
