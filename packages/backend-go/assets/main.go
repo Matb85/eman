d3da9715e75f7dae7dc298ceb6c11e5494b03507
Matb85 <69219238+Matb85@users.noreply.github.com>
2021-08-05 19:34:35 +0200

    refactor(api): :truck: move the upload func to upload.go

--- packages/backend-go/assets/main.go
+++ packages/backend-go/assets/main.go
@@ -1,17 +1,12 @@
 package assets
 
 import (
-	"io"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"redinnlabs.com/redinn-core/database"
-	"redinnlabs.com/redinn-core/utils"
 )
 
 var ASSETS_DIR string
@@ -25,7 +20,7 @@ func Setup(router *mux.Router) {
 		}
 		ASSETS_DIR = CWD + "/uploads"
 	}
-	router.HandleFunc("/assets", handlePost).Methods("POST")
+	router.HandleFunc("/assets", upload).Methods("POST")
 	router.PathPrefix("/assets/").Handler(http.StripPrefix("/api/assets/", http.FileServer(http.Dir(ASSETS_DIR)))).Methods("GET")
 }
 
@@ -37,48 +32,4 @@ type User struct {
 	LastName    string             `json:"lastname"`
 	Enterprises []string           `json:"enterprises"`
 	Folder      string             `json:"folder"`
-}
-
-func handlePost(w http.ResponseWriter, r *http.Request) {
-	ctx, cancel := utils.CreateDBContext()
-	defer cancel()
-	// auth - validate the upload token
-	uploadClaims, autherr := VerifyUploadToken(r.Header.Get("Upload"))
-	if autherr != nil {
-		w.Header().Set("Content-Type", "application/json")
-		utils.SendMessage(w, http.StatusUnauthorized, autherr.Error())
-		return
-	}
-	// get user's folder path
-	user := &User{}
-	finderr := database.UserCol.FindOne(ctx, bson.M{"_id": r.Context().Value(utils.User_id)}).Decode(user)
-	if finderr != nil {
-		utils.SendMessage(w, http.StatusBadRequest, "could not find the user: "+finderr.Error())
-	}
-	// Parse request body as multipart form data with 32MB maxrmemory
-	err := r.ParseMultipartForm(32 << 20)
-	if err != nil {
-		utils.SendMessage(w, http.StatusBadRequest, "could not parse the body: "+err.Error())
-	}
-	// Get the file from Form
-	file, handler, err := r.FormFile("file")
-	if err != nil {
-		utils.SendMessage(w, http.StatusBadRequest, "could not read from the \"file\" key: "+err.Error())
-		return
-	}
-	defer file.Close()
-	// Create file locally
-	dst, err := os.Create(ASSETS_DIR + user.Folder + uploadClaims.Hash + filepath.Ext(handler.Filename))
-	if err != nil {
-		utils.SendMessage(w, http.StatusInternalServerError, "could not create the file: "+err.Error())
-		return
-	}
-	defer dst.Close()
-	// Copy the uploaded file data to the newly created file on the filesystem
-	if _, err := io.Copy(dst, file); err != nil {
-		utils.SendMessage(w, http.StatusInternalServerError, "could not copy the file data: "+err.Error())
-		return
-	}
-
-	utils.SendMessage(w, http.StatusOK, "successfully uploaded a file! ")
 }
